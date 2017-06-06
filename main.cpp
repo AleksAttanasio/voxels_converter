deb9451d2fc0fbf5a1d969ac52c3fdbc3a619218
AleksAttanasio <aleks.attanasio@gmail.com>
2017-06-06 17:34:33 +0200

    Added RegionGrowing segmentation method

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "BoxelsConversion.h"
+
+int main() {
+
+    BoxelsConversion bc;
+    Clusters segmented_pcl;
+
+    segmented_pcl = bc.RegionGrowingSegment("obj_0.pcd", 5.0, 1.5, 300);
+
+
+    return 0;
+}
